8ae7ff4ecef8972e12143c7047d61582f558fec6
UniverseCastle <woo8618@gmail.com>
2024-07-26 16:09:26 +0900

    MariaDB_ver

---
+++ src/main/java/com/planner/mapper/VoteMapper.java
@@ -0,0 +1,36 @@
+package com.planner.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import com.planner.dto.request.team.vote.VoteDTO;
+import com.planner.dto.request.team.vote.VoteInfoDTO;
+
+@Mapper
+@Repository
+public interface VoteMapper {
+
+	// 투표 생성(투표 정보)
+	public void voteInsert(VoteDTO dto);
+
+	// 투표 생성(투표 항목)
+	public void voteItemInsert(@Param("vote_id")Long vote_id, 
+							@Param("vote_item_name") String vote_item_name);
+
+	// 투표 정보 읽기
+	public VoteInfoDTO voteInfo(Long vote_id);
+
+	// 투표 유무 확인
+	public int voteCheck(@Param("vote_id") Long vote_id,
+						@Param("team_member_id")Long team_member_id);
+
+	// 투표 인원 추가
+	public int voteMemberInsert(@Param("vote_item_id") Long vote_item_id,
+								@Param("team_member_id")Long team_member_id);
+	
+	// vote 시퀀스 찾기
+	public Long findVoteId(@Param("team_board_id") Long team_board_id);
+}
